77a87341cd0484bcf74ae2cd81a4304ccd815063
Pride Mokhele <pride.mokhele@gmail.com>
2014-06-26 13:43:25 +0200

    * added table and comlumn retreiving functions

--- libs/fns/db_interrogator.php
+++ libs/fns/db_interrogator.php
@@ -1,15 +1,14 @@
 <?php
+include_lib('db_connect_fns');
 class DbInterrogator
 {
     public function run_sql($query)
     {
         $result = mysql_query($query);
-
         if(!$result)
         {
-            die('Could not retrieve data: ' . mysql_error() . "\n". $query);
+            die('Could not execute query: ' . mysql_error() . "\n". $query);
         }
-        
         $data = array();
         while($row = mysql_fetch_array($result))
         {
@@ -27,10 +26,39 @@ class DbInterrogator
         else
         {
             $comma_separated = implode(', ' . $columns);
-            $sql = 'SELECT ' .$comma_separated. ' * FROM '. $table;
+            $sql = 'SELECT ' .$comma_separated. ' FROM '. $table;
         }
         $result = $this->run_sql($sql);
         return $result;
+    }
+
+    public function db_tables($db)
+    {
+        $sql = 'SHOW TABLES FROM ' . $db;
+        $result = $this->run_sql($sql);
+        foreach($result as $table) {$tables[] = $table[0];}
+        return $tables;
+    }
+
+    public function table_columns($table)
+    {
+        $sql = 'DESCRIBE ' . $table;
+        $result = $this->run_sql($sql);
+        $fields = array();
+        foreach($result as $columns) {$fields[] = $columns['Field'];}
+        return $fields;
+    }
+
+    public function insert_data($user_id, $entry_text)
+    {
+        $entry_text = mysql_real_escape_string($entry_text);
+
+        $sql = 	"INSERT INTO blog".
+            "(user_id, blog_text) ".
+            "VALUES($user_id, '$entry_text')";
+
+        $this->run_sql($sql);
+        return true;
     }
 }
 ?>
