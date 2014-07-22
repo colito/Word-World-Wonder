1e9f97427bad25cc38dccbea6a21622e968534a8
Pride Mokhele <pride.mokhele@gmail.com>
2014-07-22 15:52:26 +0200

    *

--- content/word/add-piece.php
+++ content/word/add-piece.php
@@ -1,13 +1,20 @@
 [page:Add Your Piece]
+<?php
+if(!empty($_POST['blog_text']))
+{
+    $entry_text = $_POST['blog_text'];
+    var_dump($entry_text);
+}
+?>
 
     <div id="heading" class="grid_12">
         <h3>Add Your Piece</h3>
     </div>
 
     <div id="autor_area" class="grid_8">
-        <form method="post">
+        <form method="post" action="word/add-piece.php">
             <input type="text" name="piece_name" id="piece_name" placeholder="Title">
-            <textarea name=""></textarea>
+            <textarea name="blog_text"><?php if(!empty($entry_text)) { echo $entry_text;} ?></textarea>
             <input type="submit" value="Save" id="btn-save">
         </form>
     <div>
