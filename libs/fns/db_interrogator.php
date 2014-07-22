63d702b7d6001b4186940003257d06640f2df9b8
Pride Mokhele <pride.mokhele@gmail.com>
2014-07-22 15:50:33 +0200

    * switched over from MYSQL to MYSQLi

--- libs/fns/db_interrogator.php
+++ libs/fns/db_interrogator.php
@@ -1,22 +1,30 @@
 <?php
-include_lib('db_connect_fns');
 class DbInterrogator
 {
-    public function run_sql($query)
+    /*-----------=============CONNECT TO DATABASE===============----------------*/
+    public function db_connect()
     {
-        $result = mysql_query($query);
-        if(!$result)
-        {
-            die('Could not execute query: ' . mysql_error() . "\n". $query);
+        require_once('config.php');
+        $config = new Config();
+
+        $hostadress = $config->host;
+        $username = $config->user_name;
+        $password = $config->password;
+        $my_sql_db = $config->database;
+
+        $mysqi = new mysqli($hostadress, $username, $password, $my_sql_db);
+
+        if (mysqli_connect_errno()) {
+            die('Could not connect to MySQL: ' . mysqli_connect_error());
         }
-        $data = array();
-        while($row = mysql_fetch_array($result))
-        {
-            $data[] = $row;
-        }
-        return $data;
+
+        return $mysqi;
     }
 
+
+    /*-----------=============TYPICAL SQL TRANSACTIONS===============----------------*/
+
+    #Get
     public function get_data($table, $columns)
     {
         if(empty($columns))
@@ -30,6 +38,54 @@ class DbInterrogator
         }
         $result = $this->run_sql($sql);
         return $result;
+    }
+
+    #Post
+    public function insert_data($user_id, $entry_text)
+    {
+        $entry_text = mysql_real_escape_string($entry_text);
+
+        $sql = 	"INSERT INTO blog".
+            "(user_id, blog_text) ".
+            "VALUES($user_id, '$entry_text')";
+
+        $this->run_sql($sql);
+        return true;
+    }
+
+    #Update
+    public function update_data($columns = array())
+    {
+        foreach($columns as $column)
+        {
+
+        }
+
+        $sql =  'UPDATE table_name';
+        $sql .= 'SET column1 = value1, column2 = value2';
+        $sql .= 'WHERE some_column = some_value';
+
+        $result = $this->run_sql($sql);
+
+        return $result;
+    }
+
+
+    /*-----------==============MYSQL FUNCTIONS==============----------------*/
+    public function run_sql($query)
+    {
+        $mysqli = $this->db_connect();
+
+        $result = $mysqli->query($query);
+
+        if(!$result) { die('Could not execute query: ' . mysql_error() . "\n". $query); }
+
+        $data = array();
+        while($row = mysqli_fetch_array($result)) { $data[] = $row; }
+
+        $mysqli->close();
+
+        return $data;
     }
 
     public function db_tables($db)
@@ -47,18 +103,6 @@ class DbInterrogator
         $fields = array();
         foreach($result as $columns) {$fields[] = $columns['Field'];}
         return $fields;
-    }
-
-    public function insert_data($user_id, $entry_text)
-    {
-        $entry_text = mysql_real_escape_string($entry_text);
-
-        $sql = 	"INSERT INTO blog".
-            "(user_id, blog_text) ".
-            "VALUES($user_id, '$entry_text')";
-
-        $this->run_sql($sql);
-        return true;
     }
 }
 ?>
